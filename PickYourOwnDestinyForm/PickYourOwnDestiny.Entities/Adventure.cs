4fb94ad3ca941dccfa63cf4e98874f63be209fe5
Kevin Thompson <kevindthompson@live.com>
2016-01-11 20:40:11 -0800

    Story and Text complete

--- PickYourOwnDestinyForm/PickYourOwnDestiny.Entities/Adventure.cs
+++ PickYourOwnDestinyForm/PickYourOwnDestiny.Entities/Adventure.cs
@@ -15,15 +15,18 @@ namespace PickYourOwnDestiny.Entities
         public int Choice1Success { get; set; }
         public int Choice1Max { get; set; }
         public int Choice1Multiplier { get; set; }
+        public string Choice1ResultText { get; set; }
         public string Choice2ButtonText { get; set; }
         public string Choice2Stat { get; set; }
         public int Choice2Success { get; set; }
         public int Choice2Max { get; set; }
         public int Choice2Multiplier { get; set; }
+        public string Choice2ResultText { get; set; }
         public string Choice3ButtonText { get; set; }
         public string Choice3Stat { get; set; }
         public int Choice3Success { get; set; }
         public int Choice3Max { get; set; }
         public int Choice3Multiplier { get; set; }
+        public string Choice3ResultText { get; set; }
     }
 }
--- PickYourOwnDestinyForm/PickYourOwnDestiny.Entities/AlleyBrawlAdventure.cs
+++ PickYourOwnDestinyForm/PickYourOwnDestiny.Entities/AlleyBrawlAdventure.cs
@@ -17,16 +17,19 @@ namespace PickYourOwnDestiny.Entities
             this.Choice1Stat = "Dex";
             this.Choice1Max = 5;
             this.Choice1Multiplier = -1;
+            this.Choice1ResultText = "You stumble a little, but you manage to get away relatively unscathed";
             this.Choice2ButtonText = "Rush the one in front of you, it's time to brawl";
             this.Choice2Stat = "Strength";
             this.Choice2Success = 18;
             this.Choice2Max = 8;
             this.Choice2Multiplier = -2;
+            this.Choice2ResultText = "You make such bold showing that the 5 thugs don't know what to do, they take a few swings, but then run away as they see you aren't an easy target";
             this.Choice3ButtonText = "Start acting like a crazy person, then run away while they laugh";
             this.Choice3Stat = "Knowledge";
             this.Choice3Success = 20;
             this.Choice3Max = 10;
             this.Choice3Multiplier = -3;
+            this.Choice3ResultText = "At first they don't know what to think. One of them manages to hit you over the head with a club, but that just makes the rest laugh, and they walk away";
         }
     }
 }
