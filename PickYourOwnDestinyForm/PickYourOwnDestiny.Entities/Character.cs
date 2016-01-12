446f850d6ab6f5a2d4afd1dda787c771c84d2d63
Kevin Thompson <kevindthompson@live.com>
2016-01-11 17:29:30 -0800

    This is the Winform Version

---
+++ PickYourOwnDestinyForm/PickYourOwnDestiny.Entities/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickYourOwnDestiny.Entities
+{
+    public class Character
+    {
+        public string CharacterRace { get; set; }
+
+        public string CharacterClass { get; set; }
+
+        public string Name { get; set; }
+
+        public Character()
+        {
+            this.CharacterClass = String.Empty;
+            this.CharacterRace = String.Empty;
+            this.Name = String.Empty;
+        }
+    }
+}
---
+++ PickYourOwnDestinyForm/PickYourOwnDestinyFormTests/ModelTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PickYourOwnDestiny.Entities;
+
+namespace PickYourOwnDestinyFormTests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void CharacterModelExists()
+        {
+            Character c = new Character();
+            
+            Assert.AreEqual("PickYourOwnDestiny.Entities.Character", c.GetType().ToString());    
+        }
+
+        [TestMethod]
+        public void CharacterModelHasNonNullProperties()
+        {
+            Character c = new Character();
+
+            Assert.IsTrue(c.Name != null);
+            Assert.IsTrue(c.CharacterRace != null);
+            Assert.IsTrue(c.CharacterClass != null);
+        }
+
+        [TestMethod]
+        public void SaveClassExists()
+        {
+
+        }
+    }
+}
