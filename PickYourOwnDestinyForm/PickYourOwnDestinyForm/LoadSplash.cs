de90072d362c914362365dcbe8f9761781a9dd0a
quezman49 <quezman49comcast.net>
2016-02-01 19:13:52 -0800

    me marquez

---
+++ PickYourOwnDestinyForm/PickYourOwnDestinyForm/LoadSplash.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PYOD_Slpash
+{
+    public partial class LoadSplash : Form
+    {
+        public int count = 1;
+        
+        public LoadSplash()
+        {
+            InitializeComponent();
+         
+            
+            
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            splashProgressbar.Increment(count);
+            if (splashProgressbar.Value==100) 
+            {
+                timerSplash.Stop();
+                Console.WriteLine("in");
+                LoadingScreen f2 = new LoadingScreen();
+                this.Hide();
+                f2.Show();
+            }
+            else
+            {
+                //count++;
+                Console.WriteLine("hit" +  count.ToString());
+            }
+
+        }
+    }
+}
---
+++ PickYourOwnDestinyForm/PickYourOwnDestinyForm/LoadSplash.designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PYOD_Slpash
+{
+    partial class LoadSplash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadSplash));
+            this.timerSplash = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splashProgressbar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerSplash
+            // 
+            this.timerSplash.Enabled = true;
+            this.timerSplash.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1008, 733);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // splashProgressbar
+            // 
+            this.splashProgressbar.Location = new System.Drawing.Point(202, 646);
+            this.splashProgressbar.Name = "splashProgressbar";
+            this.splashProgressbar.Size = new System.Drawing.Size(794, 25);
+            this.splashProgressbar.TabIndex = 1;
+            this.splashProgressbar.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Banner", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(202, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 92);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PYOD";
+            // 
+            // LoadSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1008, 733);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.splashProgressbar);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "LoadSplash";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " Welcome to PYOD";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerSplash;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar splashProgressbar;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
