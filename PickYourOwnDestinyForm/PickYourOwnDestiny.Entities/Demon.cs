39fedb64a88c17a7c23635eb93e490061433d729
Benjamin Crawford <b.m.crawford@outlook.com>
2016-02-01 18:37:02 -0800

    Start, Load, and Conformation

---
+++ PickYourOwnDestinyForm/PickYourOwnDestiny.Entities/Demon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickYourOwnDestiny.Entities
+{
+    class Demon : Character
+    {
+        public Demon()
+        {
+            this.Name = "Ra's Al Ghul";
+            this.Strength = 15;
+            this.Knowledge = 15;
+            this.Dex = 12;
+            this.HeroPoints = 5;
+            this.HitPoints = 20;
+            this.StoryModeTracker = 1;
+
+        }
+    }
+}
--- PickYourOwnDestinyForm/PickYourOwnDestinyForm/Properties/Resources.Designer.cs
+++ PickYourOwnDestinyForm/PickYourOwnDestinyForm/Properties/Resources.Designer.cs
@@ -59,5 +59,45 @@ namespace PickYourOwnDestiny.UI.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Book_Animation {
+            get {
+                object obj = ResourceManager.GetObject("Book_Animation", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Continue {
+            get {
+                object obj = ResourceManager.GetObject("Continue", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Continue_Book_Animation {
+            get {
+                object obj = ResourceManager.GetObject("Continue_Book_Animation", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Start {
+            get {
+                object obj = ResourceManager.GetObject("Start", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
