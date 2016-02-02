b7207984d8862fecd6347abf34961d5cdd9acecb
quezman49 <quezman49comcast.net>
2016-02-01 19:30:15 -0800

    me-marquez

--- PickYourOwnDestinyForm/PickYourOwnDestinyForm/Program.cs
+++ PickYourOwnDestinyForm/PickYourOwnDestinyForm/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PickYourOwnDestiny;
+using PYOD_Slpash;
 
 namespace PickYourOwnDestiny.UI
 {
@@ -16,7 +18,7 @@ namespace PickYourOwnDestiny.UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new LoadSplash());
         }
     }
 }
